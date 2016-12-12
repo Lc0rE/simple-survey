dc51a2e2f1008249bec080d332b5859dfd38d911
Luca Corsilli <luca.corsilli@gmail.com>
2016-12-12 16:37:01 +0100

    Move script file to the js/ directory

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-$("button").click(function() {
-    alert("click event");
+$(document).ready(function() {
+    var rightArrow = $(".right-arrow");
+    var leftArrow = $(".left-arrow");
+
+    $(".right-container > button").click(function() {
+        $(".right-container").find(".blue").removeClass("blue");
+
+        $(this).toggleClass("blue");
+
+        $(rightArrow).removeAttr("disabled");
+        $(rightArrow).addClass("blue");
+
+    });
+
 });
+

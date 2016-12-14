e4dbfc74b75065b24e3d0731e6b57a4c9a747c41
Luca Corsilli <luca.corsilli@gmail.com>
2016-12-14 16:05:30 +0100

    Add JSON file with some random Lorem Ipsum text

---
+++ js/load.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  var click = 0;
+  var showData = $('#show-data');
+  var showCounter = $('#show-counter');
+
+  $.getJSON('../data/example.json', function (data) {
+    var length = data.items.length;
+    showCounter.append(click+1 + " / " + length);
+  });
+
+
+  $('#next-data').click(function () {
+    click++;
+    $.getJSON('../data/example.json', function (data) {
+      var items = data.items[click].paragraph;
+      var length = data.items.length;
+
+      showData.empty();
+      showData.append(items);
+
+      showCounter.empty();
+      showCounter.append(click+1 + " / " + length);
+    });
+  });
+
+  $('#prev-data').click(function() {
+    click--;
+    $.getJSON('../data/example.json', function(data) {
+      var items = data.items[click].paragraph;
+      var length = data.items.length;
+
+      showData.empty();
+      showData.append(items);
+
+      showCounter.empty();
+      showCounter.append(click+1 + " / " + length);
+    });
+  })
+});

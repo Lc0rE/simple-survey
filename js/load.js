cee49c77631d6ed81c0c6f450312fb2355da5a6b
Luca Corsilli <luca.corsilli@gmail.com>
2016-12-22 13:05:04 +0100

    Answers are now saved during the vote session. A refresh of the page does not keep the answers

--- js/load.js
+++ js/load.js
@@ -1,44 +1,99 @@
-$(document).ready(function () {
-  var click = 0;
-  var showData = $('#show-data');
-  var showCounter = $('#show-counter');
+$(document).ready(function() {
+			var question = 0;
+			var showData = $('#show-data');
+			var showCounter = $('#show-counter');
+			var Answers = {};
 
-  $.getJSON('../data/example.json', function (data) {
-    var length = data.items.length;
-    showCounter.append(click+1 + " / " + length);
-  });
+			$.getJSON('../data/example.json', function(data) {
+				var length = data.items.length;
+				showCounter.append(question + 1 + " / " + length);
+			});
 
 
-  $('#next-data').click(function () {
-    click++;
-    $.getJSON('../data/example.json', function (data) {
-      var items = data.items[click].paragraph;
-      var length = data.items.length;
+			$('#next-data').click(function() {
+				question++;
+				$.getJSON('../data/example.json', function(data) {
+					var items = data.items[question].paragraph;
+					var length = data.items.length;
 
-      showData.empty();
-      showData.append(items);
+					var items = data.items[question].paragraph;
+					var length = data.items.length;
 
-      showCounter.empty();
-      showCounter.append(click+1 + " / " + length);
+					showData.empty();
+					showData.append(items);
 
-      $(".right-container").find(".blue").removeClass("blue");
+					showCounter.empty();
+					showCounter.append(question + 1 + " / " + length);
 
+					$(".right-container").find(".blue").removeClass("blue");
+
+					$('#next-data').removeClass("blue");
+					$('#next-data').prop('disabled', true);
+
+          if (hasAnswer(this, question, Answers) == true) {
+            checkAnswer(this, question, Answers);
+            $('#next-data').prop('disabled', false);
+          }
+
+
+				});
+			});
+
+			$('#prev-data').click(function() {
+				question--;
+				$.getJSON('../data/example.json', function(data) {
+					var items = data.items[question].paragraph;
+					var length = data.items.length;
+
+					showData.empty();
+					showData.append(items);
+
+					showCounter.empty();
+					showCounter.append(question + 1 + " / " + length);
+
+					$(".right-container").find(".blue").removeClass("blue");
+					$('#prev-data').removeClass("blue");
+
+          if (hasAnswer(this, question, Answers) == true) {
+            checkAnswer(this, question, Answers);
+            $('#next-data').prop('disabled', false);
+          }
+
+				});
+			});
+
+
+			$(".survey-button").click(function() {
+				var id = this.id;
+				Answers[question] = id;
+
+				console.log(Answers);
+			});
+
+      function hasAnswer(button, questionNumber, answersObject) {
+        if (answersObject.hasOwnProperty(questionNumber)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+
+      function checkAnswer(button, questionNumber, answersObject) {
+				var id = button.id;
+
+				if (answersObject.hasOwnProperty(questionNumber)) {
+          var checkedButton = answersObject[questionNumber];
+
+					console.log("Question: " + questionNumber + " ==> " + answersObject[questionNumber]);
+					$("#"+checkedButton).addClass("blue");
+
+          return true;
+				} else {
+
+          return false;
+        }
+
+
+				console.log(Answers);
+			}
     });
-  });
-
-  $('#prev-data').click(function() {
-    click--;
-    $.getJSON('../data/example.json', function(data) {
-      var items = data.items[click].paragraph;
-      var length = data.items.length;
-
-      showData.empty();
-      showData.append(items);
-
-      showCounter.empty();
-      showCounter.append(click+1 + " / " + length);
-
-      $(".right-container").find(".blue").removeClass("blue");
-    });
-  })
-});
